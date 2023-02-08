7c65349b3bc2e8eac0deb167fdf09fab31ece853
kareefardi <kareem.farid@efabless.com>
2023-02-08 18:35:58 +0200

    already know where volare is not need to try and source bashrc

--- pdk.bash
+++ pdk.bash
@@ -6,10 +6,11 @@ sudo apt install -y python3-pip
 
 pip3 install volare
 
-latest=$(python3 -c "print($(volare ls-remote)[0])")
+
+latest=$(python3 -c "print($($HOME/.local/volare ls-remote)[0])")
+
+bash -c "$HOME/.local/bin/volare enable ${latest} --pdk-root $HOME/pdks"
 
 new_path='export PATH=$PATH:$HOME/.local/bin'
 grep -qxF "$new_path" $HOME/.bashrc || echo "$new_path" >> $HOME/.bashrc
-
-bash -c "volare enable ${latest} --pdk-root $HOME/pdks"
 
