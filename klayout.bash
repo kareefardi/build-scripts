01f173081147bd6535187ab7c368e2d2292667f9
kareefardi <kareem.farid@efabless.com>
2023-02-08 19:14:29 +0200

    fix variable reference

--- klayout.bash
+++ klayout.bash
@@ -3,6 +3,7 @@
 sudo apt install -y $(cat ./dependencies.txt)
 
 deb="klayout_0.28.5-1_amd64.deb"
+rm -rf ${HOME}/${deb}
 wget https://www.klayout.org/downloads/Ubuntu-22/${deb} -P ${HOME}
-sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME}/{deb}; }
+sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME}/${deb} ; }
 
