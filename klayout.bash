0d4dc09cfdf18d66f2f8170e2eb5fce3ae9b82d9
kareefardi <kareem.farid@efabless.com>
2023-02-08 19:15:28 +0200

    apt install -y

--- klayout.bash
+++ klayout.bash
@@ -1,9 +1,11 @@
 #!/bin/env bash
 
+set -eux
+
 sudo apt install -y $(cat ./dependencies.txt)
 
 deb="klayout_0.28.5-1_amd64.deb"
 rm -rf ${HOME}/${deb}
 wget https://www.klayout.org/downloads/Ubuntu-22/${deb} -P ${HOME}
-sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME}/${deb} ; }
+sudo dpkg -i ${HOME}/${deb} || { sudo apt -fy install && sudo dpkg -i ${HOME}/${deb} ; }
 
