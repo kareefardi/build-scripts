39233944ebe96116698710c12741a483c05980f5
kareefardi <kareem.farid@efabless.com>
2023-02-08 19:11:25 +0200

    add klayout install script

---
+++ klayout.bash
@@ -0,0 +1,8 @@
+!#/bin/env bash
+
+sudo apt install -y $(cat ./dependencies.txt)
+
+deb="klayout_0.28.5-1_amd64.deb"
+wget https://www.klayout.org/downloads/Ubuntu-22/${deb} -P ${HOME}
+sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME} {deb}; }
+
