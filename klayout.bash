180485bc827a9773e407e507c2367ecf3d44e115
kareefardi <kareem.farid@efabless.com>
2023-02-08 19:12:55 +0200

    fix path typo

--- klayout.bash
+++ klayout.bash
@@ -4,5 +4,5 @@ sudo apt install -y $(cat ./dependencies.txt)
 
 deb="klayout_0.28.5-1_amd64.deb"
 wget https://www.klayout.org/downloads/Ubuntu-22/${deb} -P ${HOME}
-sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME} {deb}; }
+sudo dpkg -i ${HOME}/${deb} || { sudo apt -f install && sudo dpkg -i ${HOME}/{deb}; }
 
